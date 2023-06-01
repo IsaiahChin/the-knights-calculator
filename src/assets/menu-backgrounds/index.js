7b94a4b945dae68cc15eb759d1c5b84594666a75
Isaiah Chin <68924638+IsaiahChin@users.noreply.github.com>
2023-06-01 23:49:51 +1200

    Added menu backgrounds

---
+++ src/assets/menu-backgrounds/index.js
@@ -0,0 +1,25 @@
+import classicBg from './classic.png';
+import godmasterBg from './godmaster.png';
+import hiddenDreamsBg from './hidden-dreams.png';
+import infectedBg from './infected.png';
+import lifebloodBg from './lifeblood.png';
+import originalBg from './original.png';
+import steelSoulBg from './steel-soul.png';
+import theEternalOrdealBg from './the-eternal-ordeal.png';
+import theGrimmTroupeBg from './the-grimm-troupe.png';
+import voidBg from './void.png';
+import voidheartBg from './voidheart.png';
+
+export default {
+  classicBg,
+  godmasterBg,
+  hiddenDreamsBg,
+  infectedBg,
+  lifebloodBg,
+  originalBg,
+  steelSoulBg,
+  theEternalOrdealBg,
+  theGrimmTroupeBg,
+  voidBg,
+  voidheartBg,
+};
