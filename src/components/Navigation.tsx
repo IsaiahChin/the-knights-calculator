9464f74cbabd76be4ebff9768bae6eacf2920d30
Isaiah Chin <68924638+IsaiahChin@users.noreply.github.com>
2023-06-09 16:16:40 +1200

    Added dynamic links + styling

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,26 +2,46 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+
 import kingsBrand from '../assets/ui/kings-brand.png';
-import charmNotch from '../assets/ui/charm-notch.png';
+import journal from '../assets/ui/journal-prompt.png';
+import { creditsFleur } from '../assets/ui/fleur/';
 
 export default function Navigation() {
+  const pathname = usePathname();
+
+  const navLinks = [
+    { title: 'The Knight', link: '/' },
+    { title: 'Enemies', link: '/enemies' },
+  ];
+
   return (
-    <nav className="w-full max-h-[10rem] px-3 p-4 flex items-center justify-between">
-      <Link href="/">
-        <Image
-          src={kingsBrand}
-          alt={`The Knight's Calculator`}
-          className="aspect-auto w-auto max-w-[60px] h-auto cursor-pointer"
-        />
-      </Link>
-      <button type="button">
-        <Image
-          src={charmNotch}
-          alt="Settings"
-          className="aspect-auto w-auto max-w-[60px] h-auto cursor-pointer"
-        />
-      </button>
-    </nav>
+    <>
+      <nav className="w-full max-h-[10rem] px-3 p-4 flex items-center justify-between">
+        <Link href="/" className="glow-on-hover">
+          <Image
+            src={kingsBrand}
+            alt={`The Knight's Calculator`}
+            className="w-auto max-w-[60px] cursor-pointer"
+          />
+        </Link>
+        <ul className="inline-flex justify-center gap-4 [&_a]:glow-on-hover">
+          {navLinks.map((link, index) => (
+            <li key={index} className={`${pathname == link.link && 'glow'}`}>
+              <Link href={link.link}>{link.title}</Link>
+            </li>
+          ))}
+        </ul>
+        <button type="button">
+          <Image
+            src={journal}
+            alt="Settings"
+            className="w-auto max-w-[60px] cursor-pointer"
+          />
+        </button>
+      </nav>
+      <Image src={creditsFleur} alt="Nav border" className="m-[0_auto]" />
+    </>
   );
 }
