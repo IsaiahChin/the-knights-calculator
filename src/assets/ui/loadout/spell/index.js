d64477aefb758f618d776819015bb8bd1de4ba57
Isaiah Chin <68924638+IsaiahChin@users.noreply.github.com>
2023-06-09 16:17:45 +1200

    Added ui + loadout assets

---
+++ src/assets/ui/loadout/spell/index.js
@@ -0,0 +1,15 @@
+import fireball from './vengeful-spirit.png';
+import fireballUpgrade from './shade-soul.png';
+import wraiths from './howling-wraiths.png';
+import wraithsUpgrade from './abyss-shriek.png';
+import dive from './desolate-dive.png';
+import diveUpgrade from './descending-dark.png';
+
+export {
+  fireball,
+  fireballUpgrade,
+  wraiths,
+  wraithsUpgrade,
+  dive,
+  diveUpgrade,
+};
