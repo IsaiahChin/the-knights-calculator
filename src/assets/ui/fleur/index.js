b18a7b95b79f3e658c51b701e7938fa3781889a5
Isaiah Chin <68924638+IsaiahChin@users.noreply.github.com>
2023-06-22 14:23:39 +1200

    Removed unused assets

--- src/assets/ui/fleur/index.js
+++ src/assets/ui/fleur/index.js
@@ -1,7 +1,5 @@
 import bottomFleur from './bottom-fleur.png';
 import creditsFleur from './credits-fleur.png';
-import ggBoardBottom from './gg-board-bottom.png';
-import ggBoardTop from './gg-board-top.png';
 import pauseTopFleur from './pause-top-fleur.png';
 
-export { bottomFleur, creditsFleur, ggBoardTop, ggBoardBottom, pauseTopFleur };
+export { bottomFleur, creditsFleur, pauseTopFleur };
