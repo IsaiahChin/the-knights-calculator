d3b938a7e0868a4535526de44e47e5767d67152e
Isaiah Chin <68924638+IsaiahChin@users.noreply.github.com>
2023-06-28 15:19:16 +1200

    Added content to Info popup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 'use client';
 
-import { DOMElement, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import InfoIcon from '@/assets/meta/InfoIcon';
+import CloseIcon from '@/assets/meta/CloseIcon';
 import { pauseTopFleur, bottomFleur } from '@/assets/ui/fleur';
 
 export default function Header() {
@@ -13,6 +15,12 @@ export default function Header() {
     setIsInfoShowing(!isInfoShowing);
   }
 
+  useEffect(() => {
+    document.getElementsByTagName('body')[0].style.overflowY = isInfoShowing
+      ? 'hidden'
+      : '';
+  });
+
   return (
     <header>
       <button
@@ -20,7 +28,7 @@ export default function Header() {
         className="z-[100] absolute top-4 right-4 p-4 cursor-pointer"
         onClick={() => handleInfoChange()}
       >
-        <InfoIcon />
+        {isInfoShowing ? <CloseIcon /> : <InfoIcon />}
       </button>
       <div
         id="blur-overlay"
@@ -28,20 +36,29 @@ export default function Header() {
           isInfoShowing ? 'block' : 'hidden'
         } fixed top-0 left-0 z-40 w-full h-full bg-zinc-950 bg-opacity-95 backdrop-blur-sm flex justify-center items-center`}
       >
-        <div id="center" className="text-center w-1/3 h-1/2 -top-[10%]">
+        <div id="center" className="text-center w-1/3 h-1/2">
           <div id="info-box" className="flex flex-col items-center">
-            <Image
-              src={pauseTopFleur}
-              alt="Top Fluer"
-              className="max-w-[350px]"
-            />
-            <h1 className="w-max mt-4">The Knight's Calculator</h1>
-            <p className="h-32"></p>
-            <Image
-              src={bottomFleur}
-              alt="Bottom Fluer"
-              className="max-w-[250px]"
-            />
+            <Image src={pauseTopFleur} alt="" className="max-w-[350px]" />
+            <h1 className="w-max my-4">The Knight's Calculator</h1>
+            <p className="w-max h-32">
+              A damage analysis tool for Hollow Knight
+            </p>
+            <p className="w-max [&_a]:underline [&_a]:underline-offset-2 text-xs">
+              Built by{' '}
+              <Link href="https://www.isaiahchin.com/" target="_blank">
+                Isaiah
+              </Link>
+              .{' '}
+              <Link href="https://www.hollowknight.com/" target="_blank">
+                Hollow Knight
+              </Link>{' '}
+              by{' '}
+              <Link href="https://www.teamcherry.com.au/" target="_blankF">
+                Team Cherry
+              </Link>
+              .
+            </p>
+            <Image src={bottomFleur} alt="" className="max-w-[250px] mt-4" />
           </div>
         </div>
       </div>
