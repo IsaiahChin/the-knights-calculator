9e97d7fe20e379ef255549954fb60348c5694bb6
Isaiah Chin <68924638+IsaiahChin@users.noreply.github.com>
2023-06-29 18:08:30 +1200

    Removed Fragments

--- src/components/StatContainer.tsx
+++ src/components/StatContainer.tsx
@@ -6,101 +6,97 @@ import soulOrb from '@/assets/ui/soul-orb.png';
 
 export default function StatContainer({ loadout }: { loadout: any }) {
   return (
-    <>
-      <div className="h-max p-4">
-        <div id="stats" className="h-[70vh] flex flex-col">
-          <div className="grid grid-cols-2 lg:grid-cols-[0.6fr_repeat(2,_1fr)] grid-rows-2 gap-y-12 mt-4">
-            <section className="hidden lg:flex items-center justify-center">
-              <Image
-                src={loadout.nail.image}
-                alt="Nail"
-                className="max-w-[50px]"
-              />
-            </section>
-            <StatWrapper>
-              <StatBlock
-                value={loadout.nail.damage}
-                valueSize="large"
-                subValue="dmg"
-              />
-              <StatBlock
-                value={loadout.nail.swingRate}
-                valueSize="large"
-                subValue="s swing speed"
-              />
-              <StatBlock
-                value={(loadout.nail.damage / loadout.nail.swingRate).toFixed(
-                  2
-                )}
-                valueSize="large"
-                subValue="dps"
-              />
-            </StatWrapper>
-            <StatWrapper>
-              {NAILART.map((nailart, index) => {
-                return (
-                  <StatBlock
-                    key={index}
-                    icon={nailart.icon}
-                    iconAlt={nailart.name + ' slash'}
-                    value={loadout.nail.damage * nailart.damageMod}
-                    valueSize="medium"
-                    subValue={`dmg ${
-                      nailart.name == 'cyclone'
-                        ? ` * (${nailart.hits?.min} - ${nailart.hits?.max})`
-                        : ''
-                    }`}
-                  />
-                );
-              })}
-            </StatWrapper>
-            <section className="hidden lg:flex items-center justify-center">
-              <Image src={soulOrb} alt="Soul Orb" />
-            </section>
-            <StatWrapper>
-              <StatBlock
-                value={loadout.soul.max}
-                valueSize="large"
-                subValue="max"
-              />
-              <StatBlock
-                value={loadout.soul.spellCost}
-                valueSize="large"
-                subValue="per spell"
-              />
-              <StatBlock
-                value={loadout.soul.regen}
-                valueSize="large"
-                subValue="regen on nail hit"
-              />
-            </StatWrapper>
-            <StatWrapper>
-              <StatBlock
-                icon={loadout.spell.fireball.icon}
-                iconAlt="fireball"
-                value={loadout.spell.fireball.damage}
-                valueSize="medium"
-                subValue="dmg"
-              />
-              <StatBlock
-                icon={loadout.spell.dive.icon}
-                iconAlt="dive"
-                value={loadout.spell.dive.damage}
-                valueSize="medium"
-                subValue="dmg"
-              />
-              <StatBlock
-                icon={loadout.spell.wraiths.icon}
-                iconAlt="wraiths"
-                value={loadout.spell.wraiths.damage}
-                valueSize="medium"
-                subValue="dmg"
-              />
-            </StatWrapper>
-          </div>
+    <div className="h-max p-4">
+      <div id="stats" className="h-[70vh] flex flex-col">
+        <div className="grid grid-cols-2 lg:grid-cols-[0.6fr_repeat(2,_1fr)] grid-rows-2 gap-y-12 mt-4">
+          <section className="hidden lg:flex items-center justify-center">
+            <Image
+              src={loadout.nail.image}
+              alt="Nail"
+              className="max-w-[50px]"
+            />
+          </section>
+          <StatWrapper>
+            <StatBlock
+              value={loadout.nail.damage}
+              valueSize="large"
+              subValue="dmg"
+            />
+            <StatBlock
+              value={loadout.nail.swingRate}
+              valueSize="large"
+              subValue="s swing speed"
+            />
+            <StatBlock
+              value={(loadout.nail.damage / loadout.nail.swingRate).toFixed(2)}
+              valueSize="large"
+              subValue="dps"
+            />
+          </StatWrapper>
+          <StatWrapper>
+            {NAILART.map((nailart, index) => {
+              return (
+                <StatBlock
+                  key={index}
+                  icon={nailart.icon}
+                  iconAlt={nailart.name + ' slash'}
+                  value={loadout.nail.damage * nailart.damageMod}
+                  valueSize="medium"
+                  subValue={`dmg ${
+                    nailart.name == 'cyclone'
+                      ? ` * (${nailart.hits?.min} - ${nailart.hits?.max})`
+                      : ''
+                  }`}
+                />
+              );
+            })}
+          </StatWrapper>
+          <section className="hidden lg:flex items-center justify-center">
+            <Image src={soulOrb} alt="Soul Orb" />
+          </section>
+          <StatWrapper>
+            <StatBlock
+              value={loadout.soul.max}
+              valueSize="large"
+              subValue="max"
+            />
+            <StatBlock
+              value={loadout.soul.spellCost}
+              valueSize="large"
+              subValue="per spell"
+            />
+            <StatBlock
+              value={loadout.soul.regen}
+              valueSize="large"
+              subValue="regen on nail hit"
+            />
+          </StatWrapper>
+          <StatWrapper>
+            <StatBlock
+              icon={loadout.spell.fireball.icon}
+              iconAlt="fireball"
+              value={loadout.spell.fireball.damage}
+              valueSize="medium"
+              subValue="dmg"
+            />
+            <StatBlock
+              icon={loadout.spell.dive.icon}
+              iconAlt="dive"
+              value={loadout.spell.dive.damage}
+              valueSize="medium"
+              subValue="dmg"
+            />
+            <StatBlock
+              icon={loadout.spell.wraiths.icon}
+              iconAlt="wraiths"
+              value={loadout.spell.wraiths.damage}
+              valueSize="medium"
+              subValue="dmg"
+            />
+          </StatWrapper>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
