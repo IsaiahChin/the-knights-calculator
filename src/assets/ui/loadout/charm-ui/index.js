c927493f6028e212046116f9b9789ae414c9e554
Isaiah Chin <68924638+IsaiahChin@users.noreply.github.com>
2023-07-04 17:33:28 +1200

    Added overcharm asset

---
+++ src/assets/ui/loadout/charm-ui/index.js
@@ -0,0 +1,6 @@
+import overcharmedBackboard from './overcharm-backboard.png';
+import charmNotch from './charm-notch.png';
+import notchUnlit from './charm-cost-unlit.png';
+import notchLit from './charm-cost-lit.png';
+
+export { overcharmedBackboard, charmNotch, notchUnlit, notchLit };
